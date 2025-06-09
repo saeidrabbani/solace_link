8090fe9d8641a336d88d40f0f3a3fd2196078267
saeidrabbani <saeid_rabbani@hotmail.com>
2025-06-09 21:17:14 +0200

    Create sendMessage.js

---
+++ netlify/functions/sendMessage.js
@@ -0,0 +1,20 @@
+const token = "7816762363:AAEk86WceNctBS-Kj3deftYqaD0kmb543AA";
+const chat_id = "253893212";
+
+exports.handler = async (event) => {
+    const message = event.queryStringParameters.message || "No message given";
+
+    const url = `https://api.telegram.org/bot${token}/sendMessage`;
+    const res = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ chat_id, text: message })
+    });
+
+    const data = await res.json();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ success: true, data })
+    };
+};
