cc1ca44a81ca224e351cc9a578b02cc32dfa30d0
saeidrabbani <saeid_rabbani@hotmail.com>
2025-06-23 21:58:22 +0200

    Update getlatestmessage.js

--- netlify/functions/getlatestmessage.js
+++ netlify/functions/getlatestmessage.js
@@ -1,5 +1,3 @@
-const fetch = require("node-fetch");
-
 exports.handler = async function(event, context) {
   try {
     const response = await fetch("https://solacebot.onrender.com/latest-message");
