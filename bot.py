8d9a744af0c54c825d0636321dcea9219f079a9a
saeidrabbani <saeid_rabbani@hotmail.com>
2025-07-14 15:34:36 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -87,5 +87,5 @@ def send_message(chat_id, text):
     requests.post(url, json=payload)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=True)
